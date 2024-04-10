e0d6f54ed4100a99a78091b9d77f29638547f74b
Ayush Kushwaha <58135144+ayushkx@users.noreply.github.com>
2024-04-10 12:56:11 +0530

    Update script.sh

--- AWS_resources_tracker/script.sh
+++ AWS_resources_tracker/script.sh
@@ -1,1 +1,35 @@
+#!/bin/bash
 
+#############################
+#
+# Author: Ayush Kushwaha
+# Date: 04/04/2024
+# Version: V1
+#
+# This script will report on resources usage of AWS 
+#
+#############################
+
+# debug mode
+set -x
+
+# AWS S3
+# AWS EC2
+# AWS Lambda
+# AWS IAM Users
+
+# List S3
+echo " print the value of S3 buckets"
+aws s3 ls
+
+# List EC2
+echo " print the value of EC2 instances "
+aws ec2 describe-instances
+
+# List Lambda
+echo " print the value of Lambda functions "
+aws lambda list-functions
+
+# List Iam 
+echo " print the value of Iam users "
+aws iam list-users
